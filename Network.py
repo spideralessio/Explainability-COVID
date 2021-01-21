5ff82b0ea32cc9c3b29111e1e5dcdd38ae384282
dylansavoia <dylansavoia@gmail.com>
2021-01-21 12:10:27 +0100

    Clean Up Round1

---
+++ Network.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, n_feats):
+        super(Net, self).__init__()
+        self.atom_embedding = nn.Linear(n_feats, n_feats)
+        self.conv1 = SAGEConv(n_feats, n_feats)
+        self.pool1 = EdgePooling(n_feats)
+        self.conv2 = SAGEConv(n_feats, n_feats)
+        self.pool2 = EdgePooling(n_feats)
+        self.linear = torch.nn.Linear(2*n_feats, 1)
+
+    def forward(self, x, edge_index, batch=None):
+        if len(x.shape) == 3: #NEEDED FOR EXAI, NOTICE THAT IT MUST BE ONLY ONE MOL
+            data_list = []
+            for x_i, edge_index_i in zip(x, edge_index):
+                data_list.append(Data(x=x_i, edge_index=edge_index_i))
+            data = Batch.from_data_list(data_list).to(device)
+            x = data.x
+            batch = data.batch
+            edge_index = data.edge_index
+        shape = x.shape
+        x = x.reshape(-1,shape[-1])
+        x = self.atom_embedding(x)
+        x = x.reshape(shape)
+        x = x.squeeze(1)
+        
+        x = F.relu(self.conv1(x, edge_index))
+
+        x, edge_index, batch, _ = self.pool1(x, edge_index, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index))
+     
+        x, edge_index, batch, _ = self.pool2(x, edge_index, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+        x = x1 + x2
+        x = self.linear(x)    
+        #x = torch.sigmoid(x)
+        x = x.squeeze(1)
+        return x
---
+++ commons.py
@@ -0,0 +1,55 @@
+atoms = [1,3,5,6,7,8,9,11,13,14,15,16,17,19,20,25,26,27,30,33,34,35,38,42,47,50,53, 78, 79, 80,83]
+CHIRAL_TYPES = len(Chem.rdchem.ChiralType.values)
+HYBRIDIZATION_TYPES = len(Chem.rdchem.HybridizationType.values)
+SEEN_ATOMS = {k:i for i, k in enumerate(sorted(atoms))}
+
+
+def atm_data(atm):
+    atomic_num = atm.GetAtomicNum()
+    atomic_num = np.eye(len(SEEN_ATOMS))[SEEN_ATOMS[atomic_num]]
+    chiral = np.eye(CHIRAL_TYPES)[int(atm.GetChiralTag())]
+    hybrid = np.eye(HYBRIDIZATION_TYPES)[int(atm.GetHybridization())]
+    charge = atm.GetFormalCharge()
+    imp_val = atm.GetImplicitValence()
+    aromatic = int(atm.GetIsAromatic())
+    isotope = atm.GetIsotope()
+    mass = atm.GetMass()
+    rad_ele = atm.GetNumRadicalElectrons()
+    ring = atm.IsInRing()
+    data = np.concatenate([atomic_num, [charge,imp_val, aromatic, isotope, mass, rad_ele, ring], chiral, hybrid])
+    return data
+
+def gen_graph(mol, act=None):
+    #print(act)
+    n = mol.GetNumAtoms()
+    atoms = []
+    #adj = np.zeros((n,n))
+    edge_index = []
+    for atm in mol.GetAtoms():
+        atoms.append(atm_data(atm))
+        idx = atm.GetIdx()
+        for neig in atm.GetNeighbors():
+            neig_idx = neig.GetIdx()
+            #adj[idx, neig_idx] = 1
+            #adj[neig_idx, idx] = 1
+            edge_index.append([idx, neig_idx])
+    edge_index = torch.tensor(np.transpose(edge_index)).long()
+    #print(edge_index)
+    x = torch.tensor(np.array(atoms)).float()
+    if act is not None:
+        act = torch.tensor([act]).float()
+    data = Data(x=x, edge_index=edge_index, y=act)
+    return data
+
+
+#######################################
+#             Utilities               #
+#######################################
+def try_catch(fn, *args, **kwargs):
+    try:
+        return fn(*args, **kwargs)
+    except Exception as e:
+        print(e, *args, **kwargs)
+        return None
+
+
